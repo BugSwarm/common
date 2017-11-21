eef21a942443d2ab5bce3b09819708fbbaca1a32
Naji Dmeiri <ndmeiri@users.noreply.github.com>
2017-11-20 17:11:53 -0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(
+    name='bugswarmcommon',
+    version='0.1',
+    description='Internal common libraries for BugSwarm components',
+    packages=['bugswarmcommon'],
+)
