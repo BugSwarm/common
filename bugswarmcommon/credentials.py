8d2121772e0c1a05134c6fb7b3117da0b167f441
Naji Dmeiri <ndmeiri@users.noreply.github.com>
2017-11-20 18:28:10 -0800

    Add credentials.py

---
+++ bugswarmcommon/credentials.py
@@ -0,0 +1,4 @@
+# DockerHub
+DOCKER_HUB_USERNAME = 'yclliu'  # TODO: change to bugswarm or BugSwarm
+DOCKER_HUB_PASSWORD = 'bugswarm'  # TODO
+DOCKER_HUB_REPO = '/'.join([DOCKER_HUB_USERNAME, 'artifacts'])  # TODO
