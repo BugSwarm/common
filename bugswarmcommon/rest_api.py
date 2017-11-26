c18aa8b10d3aef7926fd37c9dc0fc58c8d7cff26
ndmeiri <ndmeiri@users.noreply.github.com>
2017-11-26 01:28:17 -0800

    Short names for _perform_* methods

--- bugswarmcommon/rest_api.py
+++ bugswarmcommon/rest_api.py
@@ -22,7 +22,7 @@ def find_artifact(image_tag):
     if not isinstance(image_tag, str):
         raise ValueError
     log.debug('Trying to find artifact with image_tag', image_tag + '.')
-    return _perform_get(_artifact_image_tag_resource(image_tag))
+    return _get(_artifact_image_tag_resource(image_tag))
 
 
 def list_artifacts():
@@ -45,7 +45,7 @@ def find_email_subscriber(email):
     if not isinstance(email, str):
         raise ValueError
     log.debug('Trying to find email subscriber with email', email + '.')
-    return _perform_get(_email_subscriber_email_endpoint(email))
+    return _get(_email_subscriber_email_endpoint(email))
 
 
 def list_email_subscribers():
@@ -59,14 +59,14 @@ def count_email_subscribers():
 def confirm_email_subscriber(email):
     # Set confirmed to True and clear the confirmation token.
     updates = {'confirmed': True, 'confirmation_token': ''}
-    return _perform_patch(_email_subscriber_email_endpoint(email), updates)
+    return _patch(_email_subscriber_email_endpoint(email), updates)
 
 
 ###################################
 # Convenience REST methods
 ###################################
 
-def _perform_get(endpoint):
+def _get(endpoint):
     resp = requests.get(endpoint)
     if not resp.ok:
         log.error(resp.url)
@@ -74,7 +74,7 @@ def _perform_get(endpoint):
     return resp
 
 
-def _perform_post(endpoint, data):
+def _post(endpoint, data):
     headers = {'Content-Type': 'application/json'}
     resp = requests.post(endpoint, json.dumps(data), headers=headers)
     if not resp.ok:
@@ -83,9 +83,9 @@ def _perform_post(endpoint, data):
     return resp
 
 
-def _perform_patch(endpoint, data):
+def _patch(endpoint, data):
     # First get the entity's etag.
-    etag = _perform_get(endpoint).json()['_etag']
+    etag = _get(endpoint).json()['_etag']
     # Now patch the entity.
     headers = {
         'Content-Type': 'application/json',
@@ -98,7 +98,7 @@ def _perform_patch(endpoint, data):
     return resp
 
 
-def _perform_delete(endpoint):
+def _delete(endpoint):
     resp = requests.delete(endpoint)
     if not resp.ok:
         log.error(resp.url)
@@ -124,7 +124,7 @@ def _insert(entity, endpoint, singular_entity_name='entity'):
     if singular_entity_name is None:
         raise ValueError
     log.debug('Trying to insert', singular_entity_name + '.')
-    resp = _perform_post(endpoint, entity)
+    resp = _post(endpoint, entity)
     if resp.status_code == 422:
         log.error('The', singular_entity_name, 'was not added because it failed validation.')
         log.error(pprint.pformat(entity))
@@ -137,13 +137,13 @@ def _insert(entity, endpoint, singular_entity_name='entity'):
 def _list(endpoint):
     if endpoint is None:
         raise ValueError
-    return _perform_get(endpoint)
+    return _get(endpoint)
 
 
 def _count(endpoint):
     if endpoint is None:
         raise ValueError
-    resp = _perform_get(endpoint)
+    resp = _get(endpoint)
     result = resp.json()
     if result is not None and '_meta' in result and 'total' in result['_meta']:
         return result['_meta']['total']
