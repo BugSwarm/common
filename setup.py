d38d039669c056a5d47dda45a7af8cd10ceaf0a5
Naji Dmeiri <ndmeiri@users.noreply.github.com>
2017-12-19 13:59:45 -0800

    Update package description

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='bugswarmcommon',
     version='0.1',
-    description='Internal common libraries for BugSwarm components',
+    description='Library of common modules used throughout the BugSwarm toolset',
     packages=['bugswarmcommon'],
     install_requires=[
         'requests==2.18.4',
