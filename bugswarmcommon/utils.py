1da10befda43cb07729c99149cb4bde5f35e10f1
Naji Dmeiri <ndmeiri@users.noreply.github.com>
2018-01-30 13:06:36 -0800

    Add get_current_component_version_message in utils.py and clean up

--- bugswarmcommon/utils.py
+++ bugswarmcommon/utils.py
@@ -1,8 +1,20 @@
 from .shell_wrapper import ShellWrapper
 
-Commit = str
+_CommitSHA = str
 
 
-def get_current_component_version() -> Commit:
+def get_current_component_version_message(component_name: str) -> str:
+    """
+    Get a message that can be logged to indicate the version of the currently executing BugSwarm component.
+
+    :param component_name: The name of the component. This name is not used to obtain the version. Instead, it is merely
+                           used to compose the returned message.
+    :return: A string that can be logged to indicate the version of the currently executing BugSwarm component.
+    """
+    version = _get_current_component_version()[:7]
+    return 'Using version {} of {}.'.format(version, component_name)
+
+
+def _get_current_component_version() -> _CommitSHA:
     stdout, _, _ = ShellWrapper.run_commands('git rev-parse HEAD')
-    return stdout[:7]
+    return stdout
