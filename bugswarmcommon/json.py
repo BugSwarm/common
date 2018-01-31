895e18eff73e5c62baca340f4e823213fcae6e4a
Naji Dmeiri <ndmeiri@users.noreply.github.com>
2018-01-31 14:40:29 -0800

    Add convenience JSON file read and write functions

--- bugswarmcommon/json.py
+++ bugswarmcommon/json.py
@@ -1,4 +1,7 @@
 import json
+import os
+
+from typing import Optional
 
 
 class DefaultJSONEncoder(json.JSONEncoder):
@@ -7,3 +10,44 @@ class DefaultJSONEncoder(json.JSONEncoder):
     """
     def default(self, o):
         return o.__dict__
+
+
+def read_json(filepath: str):
+    """
+    Decode JSON data from a file.
+
+    :param filepath: Path to the file from which to read JSON data.
+    :raises ValueError:
+    :raises FileNotFoundError:
+    :return: The decoded JSON object.
+    """
+    if not filepath:
+        raise ValueError
+    if not os.path.isfile(filepath):
+        raise FileNotFoundError
+    with open(filepath) as f:
+        return json.load(f)
+
+
+def write_json(filepath: str, data, overwrite: bool = True, encoder: Optional[json.JSONEncoder] = None):
+    """
+    Write JSON data to a file.
+
+    :param filepath: Path to the file to which to write JSON data.
+    :param data: The JSON object to encode and write to the file.
+    :param overwrite: Whether to overwrite a file at `filepath` if one already exists. If `overwrite` is False, and a
+                      file already exists at `filepath`, FileExistsError is raised. Defaults to True.
+    :param encoder: The encoder object to use for the JSON encoding. Should be a subclass of json.JSONEncoder.
+                    DefaultJSONEncoder is used by default.
+    :raises ValueError:
+    :raises FileExistsError: When a file at `filepath` already exists and `overwrite` is False.
+    :return: True if the write to `filepath` succeeded.
+    """
+    if not filepath:
+        raise ValueError
+    if os.path.isfile(filepath) and not overwrite:
+        raise FileExistsError
+    if not data:
+        raise ValueError
+    with open(filepath, 'w') as f:
+        json.dump(data, f, cls=encoder, indent=2)
