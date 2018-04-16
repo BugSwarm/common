7ac1ff4748fcf72e235beeabf935f5c443ad9cc3
Naji Dmeiri <ndmeiri@users.noreply.github.com>
2018-04-16 12:58:27 -0700

    Revert "Update setup.py"
    This reverts commit 4d10671f19c6fe38317de35aaa529ac3ab3a0188.

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ with open('README.md') as f:
 setup(
     name='bugswarm-common',
     version='0.0.1',
-    url='https://github.com/BugSwarm/common',
-    author='BugSwarm',
-    author_email='dev.bugswarm@gmail.com',
-
     description='Library of modules used throughout the BugSwarm toolset',
     long_description=readme,
+    license='BSD',
+    author='BugSwarm',
+    author_email='dev.bugswarm@gmail.com',
+    url='https://github.com/BugSwarm/common',
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: BSD License',
