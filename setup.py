50ab7a541e049f1554172d853bb2b746a35ed463
Naji Dmeiri <ndmeiri@users.noreply.github.com>
2018-06-25 09:49:58 -0700

    Bump to version 0.1.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='bugswarm-common',
-    version='0.0.4',
+    version='0.1.0',
     url='https://github.com/BugSwarm/common',
     author='BugSwarm',
     author_email='dev.bugswarm@gmail.com',
