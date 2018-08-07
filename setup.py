6f1d346ab7d25b37715fcc1024e3e7125acb7787
Naji Dmeiri <ndmeiri@users.noreply.github.com>
2018-08-07 14:37:37 -0400

    Bump package version to 0.1.11

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='bugswarm-common',
-    version='0.1.10',
+    version='0.1.11',
     url='https://github.com/BugSwarm/common',
     author='BugSwarm',
     author_email='dev.bugswarm@gmail.com',
