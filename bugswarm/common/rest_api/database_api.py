f56a4d2944cc7fd40c27c12cbb2ce358b278cb13
Tony Xiao <tony.xiao95@icloud.com>
2019-03-21 14:51:12 -0700

    create-account

--- bugswarm/common/rest_api/database_api.py
+++ bugswarm/common/rest_api/database_api.py
@@ -1,6 +1,7 @@
 import json
 import pprint
 from datetime import date
+import secrets
 
 from typing import Dict
 from typing import Generator
@@ -407,6 +408,24 @@ class DatabaseAPI(object):
 
     def insert_account(self, account) -> Response:
         return self._insert(DatabaseAPI._accounts_endpoint(), account, 'account')
+
+    def create_account(self, email: str, role: list = None) -> Response:
+        """
+       Create an account with given email and role. Return token in Response if succeed.
+       :param email: email address.
+       :param role: default to ['user'].
+       :return: The response object.
+       """
+        token = secrets.token_urlsafe()
+        while self.filter_account_for_token(token):
+            token = secrets.token_urlsafe()
+        if role is None:
+            role = ['user']
+        account = {'email': email, 'roles': role, 'token': token, 'password':''}
+        response = self._insert(DatabaseAPI._accounts_endpoint(), account, 'account')
+        if response.ok:
+            response._content = json.dumps(account).encode('utf-8')
+        return response
 
     def find_account(self, email: str, error_if_not_found: bool = True) -> Response:
         log.debug('Trying to find account with email {}.'.format(email))
